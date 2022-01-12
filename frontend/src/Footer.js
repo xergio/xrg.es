312b9c75a6593d8f8197902c2bc73b21856a04cc
Sergio Álvarez <sergio.alvarez@digitalvirgo.com>
2022-01-12 12:28:50 +0100

    display tag

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -9,7 +9,7 @@ export default class Footer extends Component {
     <small>
         Coded by <a href="https://twitter.com/xergio">Sergio Álvarez <BsTwitter /></a>{' '}
         <span className="text-muted">¦</span>{' '}
-        Build <a href="https://sergio.am/code/xrg.es"><tt>{process.env.REACT_APP_GIT_SHA}</tt></a>.</small>
+        Build <a href="https://sergio.am/code/xrg.es"><tt>{process.env.REACT_APP_GIT_SHA}</tt></a> Tag: <a href={'https://sergio.am/code/xrg.es/src/tag/'+process.env.REACT_APP_GIT_TAG}>{process.env.REACT_APP_GIT_TAG}</a>.</small>
 </footer>
     )
   }
